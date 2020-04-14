d5da63bc4e3f6aed780852be862898a198d955b5
Eko <ekoremdev@gmail.com>
2020-04-14 14:20:41 +0400

    initial

---
+++ app/src/main/java/net/iotinn/asynctask/MyAsyncTask.java
@@ -0,0 +1,62 @@
+package net.iotinn.asynctask;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class MyAsyncTask extends AsyncTask <Void,Void, String>{
+
+    // The TextView where we will show results
+    private WeakReference<TextView> mTextView;
+    private Context mContext;
+
+    // Constructor that provides a reference to the TextView from the MainActivity
+    MyAsyncTask(TextView tv, Context context) {
+        mTextView = new WeakReference<>(tv);
+        mContext = context;
+    }
+
+    /**
+     * Runs on the background thread.
+     *
+     * @param voids No parameters in this use case.
+     * @return Returns the string including the amount of time that
+     * the background thread slept.
+     */
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        // Generate a random number between 0 and 10.
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        // Make the task take long enough that we have
+        // time to rotate the phone while it is running.
+        int s = n * 1000;
+
+        // Sleep for the random amount of time.
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        // Return a String result.
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    /**
+     * Does something with the result on the UI thread; in this case
+     * updates the TextView.
+     */
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+        Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
+    }
+}
